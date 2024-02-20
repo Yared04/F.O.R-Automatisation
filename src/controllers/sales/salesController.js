00642944c8a3ebd6d33246ac41e6cf507248b2df
Yared04 <yaredt29@gmail.com>
2024-02-20 08:04:49 +0300

    fix: changes made

--- src/controllers/sales/salesController.js
+++ src/controllers/sales/salesController.js
@@ -59,66 +59,105 @@ async function createSale(req, res) {
       },
     });
 
-    console.log("availableProducts: ",availableProducts)
+    const totalPurchaseQuantity = availableProducts.reduce((acc, item) => acc + item.purchaseQuantity, 0);
 
-    let remainingSaleQuantity = products.saleQuantity;
-    let productPurchaseIndex = 0;
-    while (remainingSaleQuantity > 0) {
-      if (productPurchaseIndex >= availableProducts.length()) {
-        throw Error("Not Engough Products for Sale!");
+    products.map(async (product)=>{
+      let remainingSaleQuantity = product.saleQuantity;
+      let productPurchaseIndex = 0;
+      if(remainingSaleQuantity > totalPurchaseQuantity){
+        return res.status(400).json({message: "Not Engough Products for Sale!"});
       }
-      const productPurchase = availableProducts[productPurchaseIndex];
-      if (productPurchase.purchaseQuantity >= remainingSaleQuantity) {
-        await prisma.saleDetail.create({
-          data: {
-            saleQuantity: remainingSaleQuantity,
-            saleUnitPrice: products.saleUnitPrice,
-            totalSales: products.saleUnitPrice * remainingSaleQuantity,
-            unitCostOfGoods: 0,
-            purchaseId: productPurchase.purchaseId,
-            declarationId: productPurchase.declarationId,
-            productId: productPurchase.productId,
-          },
-        });
-
-        await prisma.productPurchase.update({
-          where: {
-            id: productPurchase.id,
-          },
-          data: {
-            purchaseQuantity:
-              productPurchase.purchaseQuantity - remainingSaleQuantity,
-          },
-        });
-
-        remainingSaleQuantity = 0;
-      } else {
-        await prisma.saleDetail.create({
-          data: {
-            saleQuantity: remainingSaleQuantity,
-            saleUnitPrice: products.saleUnitPrice,
-            totalSales: products.saleUnitPrice * remainingSaleQuantity,
-            unitCostOfGoods: 0,
-            purchaseId: productPurchase.purchaseId,
-            declarationId: productPurchase.declarationId,
-            productId: productPurchase.productId,
-          },
-        });
-
-        await prisma.productPurchase.delete({
-          where: {
-            id: productPurchase.id,
-          },
-        });
-
-        remainingSaleQuantity -= productPurchase.purchaseQuantity;
-        productPurchaseIndex += 1;
+      while (remainingSaleQuantity > 0) {
+        const productPurchase = availableProducts[productPurchaseIndex];
+        if (productPurchase.purchaseQuantity >= remainingSaleQuantity) {
+          await prisma.saleDetail.create({
+            data: {
+              saleQuantity: remainingSaleQuantity,
+              saleUnitPrice: product.saleUnitPrice,
+              totalSales: product.saleUnitPrice * remainingSaleQuantity,
+              unitCostOfGoods: 0,
+              purchaseId: productPurchase.purchaseId,
+              declarationId: productPurchase.declarationId,
+              productId: productPurchase.productId,
+            },
+          });
+  
+          await prisma.productPurchase.update({
+            where: {
+              id: productPurchase.id,
+            },
+            data: {
+              purchaseQuantity:
+                productPurchase.purchaseQuantity - remainingSaleQuantity,
+            },
+          });
+  
+          remainingSaleQuantity = 0;
+        } else {
+          await prisma.saleDetail.create({
+            data: {
+              saleQuantity: productPurchase.purchaseQuantity,
+              saleUnitPrice: product.saleUnitPrice,
+              totalSales: product.saleUnitPrice * productPurchase.purchaseQuantity,
+              unitCostOfGoods: 0,
+              purchaseId: productPurchase.purchaseId,
+              declarationId: productPurchase.declarationId,
+              productId: productPurchase.productId,
+            },
+          });
+  
+          await prisma.productPurchase.update({
+            where: {
+              id: productPurchase.id,
+            },
+            data: {
+              purchaseQuantity:0,
+            },
+          });
+  
+          remainingSaleQuantity -= productPurchase.purchaseQuantity;
+          productPurchaseIndex += 1;
+        }
       }
-    }
+
+    })
+
 
     res.json(createdSale);
   } catch (error) {
     console.error("Error creating sale:", error);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+async function getSaleById(req, res) {
+  try {
+    const { id } = req.params;
+    const sale = await prisma.sale.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    const saleDetails = await prisma.saleDetail.findMany({
+      where: {
+        saleId: parseInt(id),
+      },
+      select: {
+        id: true,
+        saleQuantity: true,
+        saleUnitPrice: true,
+        totalSales: true,
+        unitCostOfGoods: true,
+        purchaseId: true,
+        declarationId: true,
+        productId: true,
+      },
+    });
+
+    res.json({sale, saleDetails});
+  } catch (error) {
+    console.error("Error retrieving Sale:", error);
     res.status(500).send("Internal Server Error");
   }
 }
