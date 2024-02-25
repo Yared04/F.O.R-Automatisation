15bc9fae47633d7fe63260c37e827c10a42fe85c
kalish93 <kaleabtekalign@gmail.com>
2024-02-25 16:03:44 +0300

    feat: implemented refresh token

--- src/routes/cash-of-account/accountSubTypeRoutes.js
+++ src/routes/cash-of-account/accountSubTypeRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../../middlewares/authenticate')
 
 const accountSubTypeController = require('../../controllers/cash-of-account/accountSubTypeController');
 
-router.post('/account-sub-types', accountSubTypeController.createAccountSubType);
-router.get('/account-sub-types', accountSubTypeController.getAllAccountSubTypes);
-router.get('/account-sub-types/:id', accountSubTypeController.getAccountSubTypeById);
-router.put('/account-sub-types/:id', accountSubTypeController.updateAccountSubType);
-router.delete('/account-sub-types/:id', accountSubTypeController.deleteAccountSubType);
+router.post('/account-sub-types', authenticate, accountSubTypeController.createAccountSubType);
+router.get('/account-sub-types', authenticate, accountSubTypeController.getAllAccountSubTypes);
+router.get('/account-sub-types/:id', authenticate, accountSubTypeController.getAccountSubTypeById);
+router.put('/account-sub-types/:id', authenticate, accountSubTypeController.updateAccountSubType);
+router.delete('/account-sub-types/:id', authenticate, accountSubTypeController.deleteAccountSubType);
 
 module.exports = router;
--- src/routes/store/storeRoutes.js
+++ src/routes/store/storeRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const storeController = require('../../controllers/store/storeController');
+const authenticate = require('../../middlewares/authenticate')
 
 const router = express.Router();
 
-router.get('/stores', storeController.getStores);
-router.post('/stores', storeController.createStore);
-router.put('/stores/:id', storeController.updateStore);
-router.delete('/stores/:id', storeController.deleteStore);
-router.get('/stores/:id', storeController.getStoreById);
+router.get('/stores',authenticate, storeController.getStores);
+router.post('/stores',authenticate, storeController.createStore);
+router.put('/stores/:id',authenticate, storeController.updateStore);
+router.delete('/stores/:id',authenticate, storeController.deleteStore);
+router.get('/stores/:id',authenticate, storeController.getStoreById);
 
 module.exports = router;
