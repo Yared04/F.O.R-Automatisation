071fcae02c9063b471a64059966ff44f1d52219a
kalish93 <kaleabtekalign@gmail.com>
2024-04-17 17:21:59 +0300

    feat: added seeder

--- src/controllers/provision/provisionController.js
+++ src/controllers/provision/provisionController.js
@@ -19,6 +19,7 @@ async function getProvisions(req, res) {
                 declaration: true,
                 unitCostOfGoods: true,
                 saleQuantity: true,
+                sale: true,
                 productPurchase: {
                   select:{
                     transit: true,
@@ -46,6 +47,7 @@ async function getProvisions(req, res) {
                 declaration: true,
                 unitCostOfGoods: true,
                 saleQuantity: true,
+                sale: true,
                 productPurchase: {
                   select:{
                     transit: true,
